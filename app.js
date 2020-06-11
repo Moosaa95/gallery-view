4d1e1e20d5192bab8e2b84ad44a8607160986aa3
Moosaa95 <47601417+Moosaa95@users.noreply.github.com>
2020-06-11 11:22:53 +0100

    gallery update

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-//connecting javascript to the html DOM
-const [current, imgs, opacity] = [document.querySelector('#current'), document.querySelectorAll('.imgs img'), 0.6];
 
+//getting the variable from the html
+const [current, imgs, opacity, btn] = [document.querySelector('#current'), document.querySelectorAll('.imgs img'), 0.6, document.querySelector('.btn')];
 
 imgs[0].style.opacity = opacity;
-imgs.forEach(img => img.addEventListener('click', imgClick));
+imgs.forEach(img => img.addEventListener('click', imgClicked));
 
-//getting the first image to be blur
 
-//function for img clicked 
+function imgClicked(e){
 
-function imgClick(e){
-    //resetting opacity 
-    imgs.forEach(img => img.style.opacity = 1)
+    //resetting the opacity
+    imgs.forEach(img => img.style.opacity = 1);
     current.src = e.target.src;
 
-    //adding fading class
+
+    //adding class fades
     current.classList.add('fade-in');
 
-    //removing the fadeins
+    //remving class fades
     setTimeout(() => current.classList.remove('fade-in'), 500);
-
-    //making the img blur by adding opacity
-
-    e.target.style.opacity = opacity;
+    e.target.style.opacity = opacity
 }
